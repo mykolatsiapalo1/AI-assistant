f6a6f91438611bd97a2aa52fcaa0eecb0ed38ae8
mykolatsiapalo1 <mykolatsiapalo1@gmail.com>
2024-10-18 18:51:29 +0300

    add auto scroll to bottom

--- src/app/modules/Chat.tsx
+++ src/app/modules/Chat.tsx
@@ -1,8 +1,6 @@
-"use client";
-
+import React, { useState, useRef, useEffect } from "react";
 import ChatInput from "@/app/components/chat/ChatInput";
 import Snippets from "@/app/components/chat/Snippets";
-import React, { useState } from "react";
 import { mockResponses } from "./data";
 import { getCurrentTime } from "@/lib/utils";
 import Graph from "@/app/components/chat/templates/Graph";
@@ -16,7 +14,8 @@ const Chat = () => {
     { content: string | ResponseType; time: string; sender: string }[]
   >([]);
   const [userInput, setUserInput] = useState<string>("");
-  const inputRef = React.useRef<HTMLInputElement>(null);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const messagesEndRef = useRef<HTMLDivElement>(null); // Для автоскролу
 
   const handleUserInput = () => {
     const userMessage = userInput;
@@ -33,6 +32,16 @@ const Chat = () => {
 
     setUserInput("");
   };
+
+  const scrollToBottom = () => {
+    if (messagesEndRef.current) {
+      messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages]);
 
   const renderMessageContent = (message: ResponseType) => {
     switch (message.type) {
@@ -57,7 +66,7 @@ const Chat = () => {
     <>
       <div className="w-full px-4 h-full py-10">
         <h2 className="text-2xl font-bold mb-4 text-primary ">AI Assistant</h2>
-        <div className="h-4/5 overflow-y-auto  p-2 mb-2 ">
+        <div className="h-4/5 overflow-y-auto p-2 mb-2 ">
           {messages.length > 0 ? (
             messages.map((message, index) => (
               <div
@@ -66,40 +75,35 @@ const Chat = () => {
                   message.sender === "user" ? "justify-end" : "justify-start"
                 }`}>
                 {message.sender === "user" ? (
-                  <>
-                    <div className="flex flex-col items-end bg-secondary text-white rounded-tl-lg rounded-br-lg rounded-bl-lg px-3 py-2">
-                      <span className="">{message.content as string}</span>
-                    </div>
-                  </>
+                  <div className="flex flex-col items-end bg-secondary text-white rounded-tl-lg rounded-br-lg rounded-bl-lg px-3 py-2">
+                    <span className="">{message.content as string}</span>
+                  </div>
                 ) : (
-                  <>
-                    <div className="flex items-start w-fit ">
-                      <div className="w-10 h-10 rounded-full bg-primary flex items-center justify-center mr-2">
-                        <SmartToyIcon />
-                      </div>
-                      <div className="flex flex-col w-full">
-                        <span className="font-bold text-primary">
-                          AI assistant
-                        </span>
-                        <span className="text-xs text-gray-500">
-                          {message.time}
-                        </span>
-                        <div className="mt-1 bg-[#f4f8f7] w-full p-4 rounded-lg">
-                          {renderMessageContent(
-                            message.content as ResponseType
-                          )}
-                        </div>
+                  <div className="flex items-start w-fit ">
+                    <div className="w-10 h-10 rounded-full bg-primary flex items-center justify-center mr-2">
+                      <SmartToyIcon />
+                    </div>
+                    <div className="flex flex-col w-full">
+                      <span className="font-bold text-primary">
+                        AI assistant
+                      </span>
+                      <span className="text-xs text-gray-500">
+                        {message.time}
+                      </span>
+                      <div className="mt-1 bg-[#f4f8f7] w-full p-4 rounded-lg">
+                        {renderMessageContent(message.content as ResponseType)}
                       </div>
                     </div>
-                  </>
+                  </div>
                 )}
               </div>
             ))
           ) : (
-            <div className="text-center text-primary h-full  w-full justify-center items-center flex font-bold text-2xl ">
+            <div className="text-center text-primary h-full w-full justify-center items-center flex font-bold text-2xl">
               How can I assist you today?
             </div>
           )}
+          <div ref={messagesEndRef} />{" "}
         </div>
 
         <ChatInput
